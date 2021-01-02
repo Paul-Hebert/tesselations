c5c63d1a8c42bea2af886bdb9877afb1f3020d5e
Paul Hebert <paul@cloudfour.com>
2021-01-02 12:45:41 -0800

    Use hex colors for color inputs

--- utils/tesselations/create-data.js
+++ utils/tesselations/create-data.js
@@ -1,9 +1,8 @@
 const commands = require('./commands');
 const randomBool = require('../random-bool');
+const hslToHex = require('../hsl-to-hex');
 
-function hsla({hue, saturation, lightness}) {
-  return `hsla(${hue}, ${saturation}%, ${lightness}%, 1)`;
-}
+
 
 function plusOrMinus(base, change) {
   return base + (randomBool ? 1 : -1) * change;
@@ -11,19 +10,19 @@ function plusOrMinus(base, change) {
 
 module.exports = function() {
   const fill = {
-    hue: Math.random() * 360,
-    saturation: plusOrMinus(50, Math.random() * 50),
-    lightness: plusOrMinus(50, Math.random() * 50),
+    h: Math.random() * 360,
+    s: plusOrMinus(50, Math.random() * 50),
+    l: plusOrMinus(50, Math.random() * 50),
   }
   
   // Either use the same hue or a complementary hue for the stroke.
-  let strokeHue = randomBool() ? fill.hue : fill.hue + 180;
+  let strokeHue = randomBool() ? fill.h : fill.h + 180;
   if(strokeHue > 360) strokeHue -= 360;
   
   const stroke = {
-    hue: strokeHue,
-    saturation: 20 + Math.random() * 80,
-    lightness: 30 + Math.random() * 70,
+    h: strokeHue,
+    s: 20 + Math.random() * 80,
+    l: 30 + Math.random() * 70,
   }
   
   const height = Math.round(30 + Math.random() * 130);
@@ -33,8 +32,8 @@ module.exports = function() {
     height,
     width,
     size: width,
-    fill: hsla(fill),
-    stroke: hsla(stroke),
+    fill: hslToHex(fill),
+    stroke: hslToHex(stroke),
     strokeWidth: Math.round(1 + Math.random() * Math.min(height, width) * 1/2),
     commands: commands(width, height),
     version: '1.0.0'
