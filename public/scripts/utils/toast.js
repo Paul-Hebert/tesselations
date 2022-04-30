744dcb787447057d8b0382f82973d505b6d280f5
Paul Hebert <paul.hebert@paulhebertdesigns.com>
2022-04-30 15:17:05 -0700

    Tidy and populate about page

--- public/scripts/utils/toast.js
+++ public/scripts/utils/toast.js
@@ -2,28 +2,27 @@ export function toast(element) {
   let timeout;
 
   const hide = () => {
-    element.classList.remove('is-visible');
-  }
+    element.classList.remove("is-visible");
+  };
 
   const show = (message) => {
     element.innerHTML = message;
-    element.classList.add('look-at-me');
-    element.classList.add('is-visible');
+    element.classList.add("is-visible");
 
     if (timeout) clearTimeout(timeout);
-  
+
     timeout = setTimeout(() => {
       hide();
     }, 5000);
-  }
+  };
 
-  element.addEventListener('click', () => {
+  element.addEventListener("click", () => {
     hide();
   });
 
   return {
     element,
     hide,
-    show
-  }
+    show,
+  };
 }
